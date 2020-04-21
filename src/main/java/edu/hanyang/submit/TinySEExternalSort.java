72c83bf181d0c68f3fe7cb65e467fdf55ef23f8c
gkalstn000 <gkalstn000@gmail.com>
2020-04-21 19:07:15 +0900

    add i/o

--- src/main/java/edu/hanyang/submit/TinySEExternalSort.java
+++ src/main/java/edu/hanyang/submit/TinySEExternalSort.java
@@ -1,11 +1,90 @@
 package edu.hanyang.submit;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import edu.hanyang.indexer.ExternalSort;
 
 public class TinySEExternalSort implements ExternalSort {
-	public void sort(String infile, String outfile, String tmpdir, int blocksize, int nblocks) throws IOException {
+	
+	
+	//output buffer 변수 만드는거 전역변수
+	
+	public void sort(String infile, //input file path
+					String outfile, //output file path
+					String tmpdir, //temporary dir for
+									// creating intermediate runs
+					int blocksize, //4096 or 8192 bytes
+					int nblocks) throws IOException { // available mem, block size, M
 		// complete the method
+		
+		//1. infile i/o 열기
+		//   (term, docID, pos)로 구성
+		//2. infile의 내용을 최대한 읽을수있을만큼 많이 읽어들여서 quicksort 써서 정렬하고 run에 저장
+		//3. 각 run에서 M-1개씩 한꺼번에 merge
+		//4. output buffer memory준비(Memory one block)
+		//5. M-1 개의 runs block들 에서 가장첫번째 요소 비교후 가장큰 block을 output buffer에 저장 
+		//6. if outbuffer 꽉차면 재일앞에꺼 push to disk and memory 비우기 
+		
+		// infile i/o 열기, blocksize 만큼씩
+		DataInputStream is = new DataInputStream(
+				new BufferedInputStream(
+						new FileInputStream(infile), blocksize));
+		
+		// 최종 저장될 outfile
+		DataOutputStream os = new DataOutputStream(
+				new BufferedOutputStream(
+						new FileOutputStream(outfile), blocksize));
+		
+		
+		
+		
+		
 	}
+	public void merge(int i, int j) { //인자는 run, priority queue 써서 각 run cur 제일 우선순위 output mem, 모든 input buffer merge될때까지 반복
+		 
+	}
+	
+	public void externalmergesort(int Paze) {
+		//현재 merge 해야하는 runs의 갯수(N)가 M-1보다 작은지 check
+		//if N < M-1 
+		// 작거나 같으면 이번 페이즈가 끝이므로 call merge()
+		//if N>= M 페이즈 수가 많다
+		//M-1개의 runs를 머지한(0~M-2,M-1~2(M-1)-1) 
+		//다하면 call externalmergesort
+		//pass의 갯수 log_(M-1)^N
+	}
+	
+}
+
+
+class QuickSorter {
+    public static List<Integer> quickSort(List<Integer> list) {
+        if (list.size() <= 1) return list;
+        int pivot = list.get(list.size() / 2);
+
+        List<Integer> lesserArr = new LinkedList<>();
+        List<Integer> equalArr = new LinkedList<>();
+        List<Integer> greaterArr = new LinkedList<>();
+
+        for (int num : list) {
+            if (num < pivot) lesserArr.add(num);
+            else if (num > pivot) greaterArr.add(num);
+            else equalArr.add(num);
+        }
+
+        return Stream.of(quickSort(lesserArr), equalArr, quickSort(greaterArr))
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+    }
 }
